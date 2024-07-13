3529142391d3c3581e2b1541fbdf7c9848b87466
charmingruby <mrrubycanal3105@gmail.com>
2024-07-13 11:06:58 -0300

    feat: usecase registry

--- internal/domain/notification/notification_usecase/usecase.go
+++ internal/domain/notification/notification_usecase/usecase.go
@@ -1,9 +1,26 @@
 package notification_usecase
 
-type NotificationServiceContract interface {
+import "github.com/charmingruby/push/internal/domain/notification/notification_repository"
+
+type NotificationServiceUseCase interface {
 	CreateCommunicationChannelUseCase()
 	ScheduleNotificationUseCase()
 	GetNotificationUseCase()
 	CancelNotiticationUseCase()
 	CheckAndSendNotificationUseCase()
+}
+
+func NewNotificationUseCaseRegistry(
+	notificationRepo notification_repository.NotificationRepository,
+	communicationChannelRepo notification_repository.CommunicationChannelRepository,
+) *NotificationUseCaseRegistry {
+	return &NotificationUseCaseRegistry{
+		notificationRepo:         notificationRepo,
+		communicationChannelRepo: communicationChannelRepo,
+	}
+}
+
+type NotificationUseCaseRegistry struct {
+	notificationRepo         notification_repository.NotificationRepository
+	communicationChannelRepo notification_repository.CommunicationChannelRepository
 }
