2fc6b9e89108615df5473ff37149a4cc56bdc525
charmingruby <mrrubycanal3105@gmail.com>
2024-07-17 10:51:01 -0300

    feat: get notification endpoint

---
+++ internal/infra/transport/rest/endpoint/v1/get_notification_endpoint.go
@@ -0,0 +1,51 @@
+package v1
+
+import (
+	"github.com/charmingruby/push/internal/core"
+	"github.com/charmingruby/push/internal/domain/notification/notification_dto"
+	"github.com/charmingruby/push/internal/domain/notification/notification_entity"
+	"github.com/gin-gonic/gin"
+)
+
+type GetNotificationResponse struct {
+	Message string                            `json:"message"`
+	Data    *notification_entity.Notification `json:"data"`
+}
+
+// GetNotifications godoc
+//
+//	@Summary		Gets a notification
+//	@Description	Gets a notification
+//	@Tags			Notifications
+//	@Accept			json
+//	@Produce		json
+//	@Param			id	path		string	true	"Get Notification Payload"
+//	@Success		200	{object}	GetNotificationResponse
+//	@Failure		404	{object}	Response
+//	@Failure		500	{object}	Response
+//	@Router			/notifications/{id} [get]
+func (h *Handler) getNotificationEndpoint(c *gin.Context) {
+	notificationID := c.Param("id")
+
+	dto := notification_dto.GetNotificationDTO{
+		NotificationID: notificationID,
+	}
+
+	notification, err := h.notificationService.GetNotificationUseCase(dto)
+	if err != nil {
+		resourceNotFoundErr, ok := err.(*core.ErrNotFound)
+		if ok {
+			NewResourceNotFoundError(c, resourceNotFoundErr)
+			return
+		}
+
+		NewInternalServerError(c, err)
+		return
+	}
+
+	NewOkResponse(
+		c,
+		"notification found",
+		notification,
+	)
+}
--- internal/infra/transport/rest/endpoint/v1/handler.go
+++ internal/infra/transport/rest/endpoint/v1/handler.go
@@ -33,6 +33,7 @@ func (h *Handler) Register() {
 
 		v1.POST("/communication-channels", h.createCommunicationChannelEndpoint)
 		v1.POST("/notifications", h.scheduleNotificationEndpoint)
+		v1.POST("/notifications/:id", h.getNotificationEndpoint)
 	}
 
 	h.router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
